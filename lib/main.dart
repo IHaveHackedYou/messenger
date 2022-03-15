206919159d7df0a4858c89eac5dc3b8612bc7a91
flopp <hackeranonym@protonmail.com>
2022-03-15 17:51:23 +0100

    added customfirebase package and connected flutter project with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:messenger/src/_constants/models/routes.dart';
-import 'package:messenger/src/home/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:messenger/src/authentication/screens/logged_out_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+// followed: https://www.youtube.com/watch?v=EXp0gq9kGxI
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // initialize Firebase
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  MyApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -16,10 +20,27 @@ class MyApp extends StatelessWidget {
         title: 'Messenger',
         theme: ThemeData.light(),
         darkTheme: ThemeData.dark(),
-        themeMode: ThemeMode
-            .dark, //*Temporary dark because of using old VM (ThemeMode.system)
-        initialRoute: "/",
-        onGenerateRoute: RouteGenerator.mainRoute,
-        /* home: const HomeScreen() */);
+        //Temporarily dark cause of using old VM (ThemeMode.system)
+        themeMode: ThemeMode.dark, 
+        home: FutureBuilder(
+            //* Firebase future, that initializes Firebase
+            future: _fbApp,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                print("error");
+                // TODO error screens
+                return Text(
+                    "Something went wrong: ${snapshot.error.toString()}");
+              }
+              // if data has arrived, and everything worked successfully
+              else if(snapshot.hasData){
+                return LoggedOutScreen();
+              }
+              // if Stream hasn't finished, loading
+              else{
+                // TODO loading screens
+                return Center(child: CircularProgressIndicator());
+              }
+            }));
   }
 }
